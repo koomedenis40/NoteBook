ff07f1edfadb6ae296af66c940ff3bd42e3689a0
koomedenis40 <koomedenis40@yahoo.com>
2025-02-20 16:48:30 +0300

    Error Handling in Register View

--- lib/constants/routes.dart
+++ lib/constants/routes.dart
@@ -1,4 +1,5 @@
 const loginRoute = '/login/';
 const registerRoute = '/register/';
 const notesRoute = '/notes/';
+const verifyEmailView = '/verify-email';
 
--- lib/views/verify_email.dart
+++ lib/views/verify_email.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:mynotes/constants/routes.dart';
 
 class VerifyEmailView extends StatefulWidget {
   const VerifyEmailView({super.key});
@@ -13,13 +14,26 @@ class _VerifyEmailViewState extends State<VerifyEmailView> {
   Widget build(BuildContext context) {
     return Column(
       children: [
-        const Text('Please verify your email address'),
+        const Text(
+            "We have sent you an email verification. Please check your mail to verify your acount"),
+        const Text(
+            "If you have not received a verification email yet, press the button below"),
+        TextButton(
+          onPressed: () async {
+            final user = FirebaseAuth.instance.currentUser;
+            await user?.sendEmailVerification();
+          },
+          child: const Text('Send Email Verification'),
+        ),
         TextButton(
             onPressed: () async {
-              final user = FirebaseAuth.instance.currentUser;
-              await user?.sendEmailVerification();
+              await FirebaseAuth.instance.signOut();
+              Navigator.of(context).pushNamedAndRemoveUntil(
+                registerRoute,
+                (route) => false,
+              );
             },
-            child: const Text('Send Email Verification')),
+            child: const Text('Restart'))
       ],
     );
   }
