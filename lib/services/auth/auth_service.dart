5b6df5b685f4ffff1b02582e1375635b8adcd7d7
koomedenis40 <koomedenis40@yahoo.com>
2025-02-24 12:11:00 +0300

    Adding AuthService Unit Test

--- lib/services/auth/auth_service.dart
+++ lib/services/auth/auth_service.dart
@@ -1,4 +1,3 @@
-import 'package:firebase_core/firebase_core.dart';
 import 'package:mynotes/services/auth/auth_provider.dart';
 import 'package:mynotes/services/auth/auth_user.dart';
 import 'package:mynotes/services/auth/firebase_auth_provider.dart';
