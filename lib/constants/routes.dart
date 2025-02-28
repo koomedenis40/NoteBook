17c28e5e971c44672f2815a0f4d856bd05962803
koomedenis40 <koomedenis40@yahoo.com>
2025-02-28 14:28:12 +0300

    Updating Existing Notes

--- lib/constants/routes.dart
+++ lib/constants/routes.dart
@@ -2,5 +2,5 @@ const loginRoute = '/login/';
 const registerRoute = '/register/';
 const notesRoute = '/notes/';
 const verifyEmailView = '/verify-email';
-const newNoteRoute = '/notes/new-note';
+const createOrUpdateNoteRoute = '/notes/new-note';
 
