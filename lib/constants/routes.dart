04578381af84d086dca4b62e7218b237b5f71271
koomedenis40 <koomedenis40@yahoo.com>
2025-03-06 19:27:14 +0300

    Moving to Bloc for Routing and Dialogs

--- lib/constants/routes.dart
+++ lib/constants/routes.dart
@@ -1,6 +1,2 @@
-const loginRoute = '/login/';
-const registerRoute = '/register/';
-const notesRoute = '/notes/';
-const verifyEmailView = '/verify-email';
 const createOrUpdateNoteRoute = '/notes/new-note';
 
